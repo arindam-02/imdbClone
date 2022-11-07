cb71d5431d9db70094d21fe7f9cf21045d7ff0a1
arindam-02 <das.arin.ece@gmail.com>
2022-11-08 04:42:48 +0530

    update site

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,18 +1,18 @@
 import "../footer/Footer.css";
 
 function Footer() {
-  const clock = setInterval(() => {
-    let d = new Date();
-    let date = " " + d.toDateString() + " " + d.toLocaleTimeString();
-    document.getElementById("clk").innerHTML = date;
-  }, 1000);
+  // const clock = setInterval(() => {
+  //   let d = new Date();
+  //   let date = " " + d.toDateString() + " " + d.toLocaleTimeString();
+  //   document.getElementById("clk").innerHTML = date;
+  // }, 1000);
 
   return (
     <>
       <div className="footer ">
         <p className="text-warning text-center m-0 pb-3">
           Made With ❤ by Das ||
-          <span id="clk"></span>
+          {/* <span id="clk"></span> */}
         </p>
       </div>
     </>
