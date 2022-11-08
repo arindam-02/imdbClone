0252e54851cfd6787f0f1dbf5e477162b9c123af
arindam-02 <das.arin.ece@gmail.com>
2022-11-08 17:46:00 +0530

    update site

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -30,7 +30,7 @@ function Header() {
       showConfirmButton: true,
       timer: 1500,
     }).then(() => {
-      window.location.reload(true);
+      window.location.reload("https://keen-madeleine-38053e.netlify.app/");
     });
   };
 
