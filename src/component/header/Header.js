2ead60678bd617560b124a345fd6b852955e4a3e
arindam-02 <das.arin.ece@gmail.com>
2022-11-08 17:04:42 +0530

    update site

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -30,7 +30,7 @@ function Header() {
       showConfirmButton: true,
       timer: 1500,
     }).then(() => {
-      window.location.reload(false);
+      window.location.reload(true);
     });
   };
 
